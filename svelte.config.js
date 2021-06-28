3cb63db1078892d66011b1288f9c0f644b8b06ec
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-06-28 15:54:01 -0700

    + tailwind + node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,24 @@
 import preprocess from 'svelte-preprocess';
 
+import node from '@sveltejs/adapter-node';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [preprocess({
+        "postcss": true
+    })],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter:node()
 	}
 };
 
 export default config;
+// Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+process.env.TAILWIND_MODE = dev ? "watch" : "build";
