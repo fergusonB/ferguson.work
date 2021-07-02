fdcdd155bbc1dffe5495cd03065eb32cbe8efd6f
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-07-01 17:15:52 -0700

    lib button / menu - adds proper hovering

--- src/lib/menu/button.svelte
+++ src/lib/menu/button.svelte
@@ -3,5 +3,5 @@
 </script>
 
 
-    <a class="bg-opacity-90 bg-white rounded-md h-10 w-10" 
+    <a sveltekit:prefetch class="transition bg-opacity-90 bg-white rounded-md h-10 w-10 hover:scale-125 shadow-md hover:shadow-xl" 
     href={button.link}>{button.name[0]}</a>
