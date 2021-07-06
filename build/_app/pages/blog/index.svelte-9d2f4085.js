b0a50cbc01fc12e414595922db4995fcee7dcf0b
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-07-06 16:04:14 -0700

    remove home from pwd

---
+++ build/_app/pages/blog/index.svelte-9d2f4085.js
@@ -0,0 +1,1 @@
+import{S as s,i as e,s as a,t as r,g as t,f as l,E as o,d as c}from"../../chunks/vendor-bd31e268.js";function d(s){let e;return{c(){e=r("placeholder")},l(s){e=t(s,"placeholder")},m(s,a){l(s,e,a)},p:o,i:o,o:o,d(s){s&&c(e)}}}export default class extends s{constructor(s){super(),e(this,s,null,d,a,{})}}
