55cfd55495af4e2b5201b6c28c3804d14a91d085
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-07-06 16:48:41 -0700

    adds common backpanel for all menus

--- src/routes/app/index.svelte
+++ src/routes/app/index.svelte
@@ -1,9 +1,11 @@
 <script>
     import BackPanel from '$lib/menu/back_panel.svelte';
-
+    let items = [
+    {title:'First app', image:'/menu/app/jungle.jpg',description:'this is an app',href:'te'},
+    {title:'second app', image:'/menu/app/jungle.jpg',description:'this is an app another'},
+    {title:'third app', image:'/menu/app/jungle.jpg',description:'this is an app aasdasnother'}
+]
 </script>
 
 
-<BackPanel>
-    content
-</BackPanel>
+<BackPanel {items}/>
