8876024cf3cba9f0fad45f6270c0af2a0161d87d
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-07-06 17:03:57 -0700

    Add transition for opacity to back_panel

--- src/lib/menu/back_panel.svelte
+++ src/lib/menu/back_panel.svelte
@@ -4,7 +4,7 @@ export let items
     <div class="p-3  m-auto rounded-md bg-white w-11/12 md:w-2/3 bg-opacity-70 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
         {#each items as item}
         <a href={item.href}>
-             <div class="m-3 bg-white rounded-md text-center">
+             <div class="m-3 bg-white rounded-md text-center drop-shadow-lg transition-opacity  duration-200 opacity-95 hover:opacity-100">
                  <div class="font-bold">{item.title}</div>
                  <img src={item.image} alt={item.title + ' ' + item.description}>
                  <span class="overflow-hidden">{item.description}</span>
