f735da98e17aeb50089e2b555099b4d9c2dd8971
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-07-09 10:36:35 -0700

    Add a11y friendly selection / scaling

--- src/lib/menu/menu_panel.svelte
+++ src/lib/menu/menu_panel.svelte
@@ -1,17 +1,18 @@
 <script>
-import Panel from '$lib/rear-white-panel.svelte'
-export let items
+	import Panel from '$lib/rear-white-panel.svelte';
+	export let items;
 </script>
-    <Panel grid={true}>
-        {#each items as item}
-        <a sveltekit:prefetch  href={item.href}>
-             <div class="m-3 bg-white rounded-md text-center drop-shadow-lg transition-opacity  duration-200 opacity-95 hover:opacity-100">
-                 <div class="font-bold">{item.title}</div>
-                 <img src={item.image} alt={item.title + ' ' + item.description}>
-                 <span class="overflow-hidden">{item.description}</span>
-             </div>
-            </a>
-        {/each}
-    </Panel>
-    
 
+<Panel grid={true}>
+	{#each items as item}
+		<a class="transform transition hover:scale-105  focus:scale-105" sveltekit:prefetch href={item.href}>
+			<div
+				class="m-3 bg-gradient-to-b from-white to-gray-300  rounded-md text-center drop-shadow-md "
+			>
+				<div class="font-bold">{item.title}</div>
+				<img class=" m-auto max-h-24" src={item.image} alt={item.title + ' ' + item.description} />
+				<span class="overflow-hidden">{item.description}</span>
+			</div>
+		</a>
+	{/each}
+</Panel>
--- src/routes/app/index.svelte
+++ src/routes/app/index.svelte
@@ -2,6 +2,8 @@
     import MenuPanel from '$lib/menu/menu_panel.svelte';
 
     let items = [
+    {title:'CPUfreq', image:'/app/term.png',description:'Rust application that monitors CPU frequency at the specified interval.', href:"https://github.com/fergusonB/cpufreq"},
+    {title:'Pig', image:'/app/term.png',description:'The game of Pig, written in TS, precompiled binaries available.', href:"https://github.com/fergusonB/pig"},
     {title:'IMDB Ratings Generator', image:'/app/imdb.png',description:'Generates a visual table for IMDB ratings.', href:"/app/imdb"},
 
 ]
