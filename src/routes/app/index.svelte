36862b5679371f8773932fb49487e9779537222f
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-07-08 18:32:08 -0700

    Adds legacy IMDB app to applications

--- src/routes/app/index.svelte
+++ src/routes/app/index.svelte
@@ -1,7 +1,8 @@
 <script>
     import MenuPanel from '$lib/menu/menu_panel.svelte';
+
     let items = [
-    {title:'First app', image:'/menu/app/jungle.jpg',description:'this is an app'},
+    {title:'IMDB Ratings Generator', image:'/app/imdb.png',description:'Generates a visual table for IMDB ratings.', href:"/app/imdb"},
     {title:'second app', image:'/menu/app/jungle.jpg',description:'this is an app another'},
     {title:'third app', image:'/menu/app/jungle.jpg',description:'this is an app aasdasnother this is an app aasdasnother '}
 ]
