40f8c76bd823a717ab35de6e53b647e3b6e0bae3
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-07-15 11:04:13 -0700

    Changes linux cheatsheat icon to tux

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,7 +1,7 @@
 <script>
     import MenuPanel from '$lib/menu/menu_panel.svelte';
     let items = [
-    {title:'Linux Cheatsheet', image:'/blog/sveltekit.jpg',description:'Personal linux cheatsheet, basic security, commands, tools I like.',href:"/blog/linux-cheatsheet"},
+    {title:'Linux Cheatsheet', image:'/blog/tux.gif',description:'Personal linux cheatsheet, basic security, commands, tools I like.',href:"/blog/linux-cheatsheet"},
 
 ]
     
