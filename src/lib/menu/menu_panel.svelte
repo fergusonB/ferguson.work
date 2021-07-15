0477829417550660b1a7c061364329f3cc26dd00
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-07-15 11:19:09 -0700

    Remove prefetching of apps

--- src/lib/menu/menu_panel.svelte
+++ src/lib/menu/menu_panel.svelte
@@ -15,7 +15,7 @@
 
 <Panel grid={true}>
 	{#each items as item}
-		<a target={getTarget(item)} class="transform transition hover:scale-105  focus:scale-105" sveltekit:prefetch href={item.href}>
+		<a target={getTarget(item)} class="transform transition hover:scale-105  focus:scale-105" href={item.href}>
 			<div
 				class="m-3 bg-gradient-to-b from-white to-gray-300  rounded-md text-center drop-shadow-md "
 			>
