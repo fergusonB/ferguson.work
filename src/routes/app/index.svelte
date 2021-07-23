8a6c20c9f312d3e0e20a35d194ac7e3cad55117c
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-07-23 10:47:08 -0700

    Adds chargeto to apps

--- src/routes/app/index.svelte
+++ src/routes/app/index.svelte
@@ -2,6 +2,7 @@
     import MenuPanel from '$lib/menu/menu_panel.svelte';
 
     let items = [
+    {title:'chargeto', image:'/app/term.png',description:'Rust application that controls the max charge level of a battery on linux.', href:"https://github.com/fergusonB/chargeto"},    
     {title:'CPUfreq', image:'/app/term.png',description:'Rust application that monitors CPU frequency at the specified interval.', href:"https://github.com/fergusonB/cpufreq"},
     {title:'Pig', image:'/app/term.png',description:'The game of Pig, written in TS, precompiled binaries available.', href:"https://github.com/fergusonB/pig"},
     {title:'IMDB Ratings Generator', image:'/app/imdb.png',description:'Generates a visual table for IMDB ratings.', href:"/app/imdb"},
