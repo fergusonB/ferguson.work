f2dffc8d644b8b869f70bd7a2d28cf428d858356
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-07-29 08:59:43 -0700

    Adds default styles to articles

--- src/routes/blog/linux-cheatsheet.svelte
+++ src/routes/blog/linux-cheatsheet.svelte
@@ -14,15 +14,69 @@
 	<meta name="description" content={description} />
 </svelte:head>
 
+<style>
+	article{
+		@apply ml-3;
+	}
+h1{
+	@apply text-4xl;
+	@apply underline;
+	@apply mb-5;
+}
+h2{
+	@apply text-3xl;
+	@apply my-2;
+}
+h3{
+	@apply text-2xl;
+	@apply my-2;
+}
+ol{
+	@apply list-decimal;
+	@apply ml-5;
+}
+a {
+	@apply underline;
+}
+ul{
+	@apply list-disc;
+	@apply list-inside;
+	@apply ml-2;
+}
+table{
+	@apply min-w-max;
+	@apply w-full;
+	@apply table-auto;
+}
+tr{
+	@apply leading-normal;
+
+}
+th{
+	@apply text-left;
+	background: linear-gradient(to bottom, white, grey);
+
+}
+tr{
+	@apply border-b;
+	@apply border-gray-200;
+}
+tr:nth-child(even){
+	@apply bg-gray-300;
+}
+
+</style>
+
+
 <Panel>
-	<article class="ml-3">
-		<h1 class="text-4xl underline mb-5">{title}</h1>
+	<article>
+		<h1 >{title}</h1>
 
 		
 		<p class="text-sm text-gray-700">{date}</p>
 
-        <h2 class="text-3xl my-2">Table of Contents</h2>
-            <ol  class="list-decimal ml-5 underline">
+        <h2 >Table of Contents</h2>
+            <ol>
                 <li><a href=#basic-security>Basic Security</a></li>
                 <li><a href=#useful-commands>Useful Commands</a></li>
                 <li><a href=#tools-i-like>Tools I Like</a></li>
@@ -32,40 +86,40 @@
 
 
 
-		<h2 id="basic-security" class="text-3xl my-2"> Basic Security</h2>
+		<h2 id="basic-security"> Basic Security</h2>
 
-		<h3 class="text-xl my-2">Automatic Updates</h3>
+		<h3 >Automatic Updates</h3>
 
-		<ul class="list-disc list-inside ml-2">
+		<ul >
 			<li>apt install unattended-upgrades</li>
 			<li>dpkg-reconfigure --priority=low unattended-upgrades</li>
 		</ul>
-		<h3 class="text-xl my-2">Non-root user</h3>
+		<h3 >Non-root user</h3>
 
-		<ul class="list-disc list-inside ml-2">
+		<ul >
 			<li>adduser [user]</li>
 			<li>usermod -aG sudo [user]</li>
 		</ul>
-		<h3 class="text-xl my-2">Keys</h3>
+		<h3 >Keys</h3>
 
-		<ul class="list-disc list-inside ml-2">
+		<ul >
 			<li>mkdir ~/.ssh</li>
 			<li>chmod 700 ~/.ssh</li>
 			<li>ssh-keygen -b [4096]</li>
 			<li>ssh-copy-id [user]@[ip]</li>
 		</ul>
-		<h3 class="text-xl my-2">Harden SSH</h3>
+		<h3 >Harden SSH</h3>
 
-		<ul class="list-disc list-inside ml-2">
+		<ul >
 			<li>sudo nano /etc/ssh/sshd_config</li>
 			<li>Port [22]</li>
 			<li>AddressFamily [inet]</li>
 			<li>PermitRootLogin [no]</li>
 			<li>PasswordAuthentication [no]</li>
 		</ul>
-		<h3 class="text-xl my-2">Firewall</h3>
+		<h3 >Firewall</h3>
 
-		<ul class="list-disc list-inside ml-2">
+		<ul >
 			<li>sudo apt install ufw</li>
 			<li>sudo ufw allow [port]</li>
 			<li>sudo ufw enable</li>
@@ -76,10 +130,10 @@
 			<li>reboot</li>
 		</ul>
 
-		<h2 id="useful-commands" class="text-3xl">Check Server</h2>
+		<h2 id="useful-commands">Check Server</h2>
 
-		<h3 class="text-xl my-2">Service Scanning</h3>
-		<table class="table-auto ">
+		<h3 >Service Scanning</h3>
+		<table >
 			<thead>
 				<tr>
 					<th>Command</th>
@@ -103,8 +157,8 @@
 			</tbody>
 		</table>
 
-		<h3 class="text-xl my-2">Web Enumeration (parrot)</h3>
-		<table class="table-auto">
+		<h3 >Web Enumeration (parrot)</h3>
+		<table >
 			<thead>
 				<tr>
 					<th>Command</th>
@@ -131,27 +185,29 @@
 			</tbody>
 		</table>
 
-		<h2 id="tools-i-like" class="text-3xl">Tools I Like</h2>
+		<h2 id="tools-i-like">Tools I Like</h2>
 
-		<h3 class="text-xl my-2"><a href="http://httpie.org/" target="_blank" rel="nofollow">HTTPie</a></h3>
+		<h3 ><a href="http://httpie.org/" target="_blank" rel="nofollow">HTTPie</a></h3>
 
 		<p>HTTPie is a command line HTTP client, a user-friendly cURL replacement.</p>
 		<p />
-		<h3 class="text-xl my-2">
+		<h3 >
 			<a href="https://github.com/BurntSushi/ripgrep" target="_blank" rel="nofollow">Ripgrep</a>
 		</h3>
 		<p>Ripgrep is a fast, efficient, and accurate grep alternative.</p>
 		<p>
 
 		</p>
-		<h3 class="text-xl my-2">
+		<h3 >
 			<a href="https://github.com/sharkdp/bat" target="_blank" rel="nofollow">Bat</a>
 		</h3>
 		<p>Cat clone with syntax highlighting</p>
 
-		<h3 class="text-xl my-2">
+		<h3 >
 			<a href="https://github.com/chubin/cheat.sh" target="_blank" rel="nofollow">Cheat.sh</a>
 		</h3>
 		<p><code>curl cht.sh</code> for cheatsheets to almost anything in your terminal.</p>
 	</article>
 </Panel>
+
+
