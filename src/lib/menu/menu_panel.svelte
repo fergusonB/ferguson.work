63d249f6e2af33c2a4708858692bab018f82321b
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-08-03 14:45:50 -0700

    Add prefetching to menu panel

--- src/lib/menu/menu_panel.svelte
+++ src/lib/menu/menu_panel.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Panel from '$lib/rear-white-panel.svelte';
+import Panel from '$lib/rear-white-panel.svelte';
 	export let items;
 
 	function getTarget(item){
@@ -8,14 +8,14 @@
 				return "_blank"
 			}
 		}
-		return "_self"
+		return "";
 	}
 	
 </script>
 
 <Panel grid={true}>
 	{#each items as item}
-		<a target={getTarget(item)} class="transform transition hover:scale-105  focus:scale-105" href={item.href}>
+		<a target={getTarget(item)} sveltekit:prefetch class="transform transition hover:scale-105  focus:scale-105" href={item.href}>
 			<div
 				class="m-3 bg-gradient-to-b from-white to-gray-300  rounded-md text-center drop-shadow-md "
 			>
