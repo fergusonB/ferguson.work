2b3f3daf20953d0fc0209232e735d7d473b1e962
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-08-06 12:56:14 -0700

    Remove resume + link to source

--- src/routes/app/index.svelte
+++ src/routes/app/index.svelte
@@ -2,7 +2,7 @@
     import MenuPanel from '$lib/menu/menu_panel.svelte';
 
     let items = [
-    {title:'Ferguson.work', image:'/logo-192.png',description:'The website you are currently browsing. Svelte - HTML, JS ,CSS...', href:"/"},    
+    {title:'Ferguson.work', image:'/logo-192.png',description:'The website you are currently browsing. Svelte - HTML, JS ,CSS...', href:"https://github.com/fergusonB/ferguson.work/tree/master/src"},    
     {title:'chargeto', image:'/app/term.png',description:'Rust application that controls the max charge level of a battery on linux.', href:"https://github.com/fergusonB/chargeto"},    
     {title:'CPUfreq', image:'/app/term.png',description:'Rust application that monitors CPU frequency at the specified interval.', href:"https://github.com/fergusonB/cpufreq"},
     {title:'Pig', image:'/app/term.png',description:'The game of Pig, written in TS, precompiled binaries available.', href:"https://github.com/fergusonB/pig"},
